789160e26b747287fec628fb389753f6e5ac4b08
cllightz <cllightz@gmail.com>
2018-12-19 15:25:49 +0900

    UIレイアウトを調整した。

--- LineOfBattle/LineOfBattle/ViewModels/TitleSceneUIViewModel.cs
+++ LineOfBattle/LineOfBattle/ViewModels/TitleSceneUIViewModel.cs
@@ -1,12 +1,11 @@
-﻿using LineOfBattle.Models;
-using ShootighLibrary.MVVM;
+﻿using ShootighLibrary.MVVM;
 
 namespace LineOfBattle.ViewModels
 {
     public class TitleSceneUIViewModel : SceneUIViewModelBase
     {
-        public string Title => "Line of Title";
-        public string Prompt => "Press Left Mouse Button to Start.";
+        public string Title => "Line of Battle";
+        public string Prompt => "Press Left Mouse Button to Start!";
 
         internal TitleSceneUIViewModel( ModelBase m ) : base( m ) { }
     }
