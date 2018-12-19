fb1a57e7c701234f7d0790b8343dd9d0b9ccda1e
cllightz <cllightz@gmail.com>
2018-12-19 11:17:46 +0900

    タイトルのレイアウトを修正した。

--- LineOfBattle/LineOfBattle/Scenes/TitleScene.cs
+++ LineOfBattle/LineOfBattle/Scenes/TitleScene.cs
@@ -12,14 +12,6 @@ namespace LineOfBattle.Scenes
 
         protected override void Execute( RenderTarget target )
         {
-            //var white = new RawColor4( 1, 1, 1, 1 );
-
-            //new Label( new DrawOptions( new Vector2( 0, 100 ), 50, white ), "Line of Battle" )
-            //    .Draw( target );
-
-            //new Label( new DrawOptions( new Vector2( 0, 200 ), 25, white ), "Press Left Mouse Button to Start" )
-            //    .Draw( target );
-
             if ( Mouse.Left ) {
                 GameInstance.TransitScene<BattleScene>();
             }
